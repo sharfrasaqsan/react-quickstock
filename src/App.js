578fceaaa5247921eeabd0d5ee78e60c4a2695ec
Mohamed Sharfiras <133948569+Mohamed-Sharfiras@users.noreply.github.com>
2025-08-29 22:06:16 +0530

    add item

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import Footer from "./components/Footer";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import { Toaster } from "sonner";
+import AddItem from "./components/items/AddItem";
+import NotFound from "./pages/NotFound";
+import ScrollToTop from "./utils/ScrollToTop";
 
 function App() {
   return (
@@ -18,13 +21,23 @@ function App() {
         <Routes>
           <Route path="/" element={<Dashboard />} />
 
+          <Route path="/add-item" element={<AddItem />} />
+
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </main>
 
       <Footer />
-      <Toaster />
+
+      <ScrollToTop />
+      <Toaster
+        position="top-right"
+        theme="system"
+        toastOptions={{ duration: 5000 }}
+      />
     </div>
   );
 }
--- src/components/items/AddItem.js
+++ src/components/items/AddItem.js
@@ -1,24 +1,155 @@
-import React from "react";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { useState } from "react";
+import { toast } from "sonner";
+import { db } from "../../firebase/Config";
+import { useData } from "../../contexts/DataContext";
+import ButtonSpinner from "../../utils/ButtonSpinner";
 
 const AddItem = () => {
+  const { setItems } = useData();
+
+  const [name, setName] = useState("");
+  const [stock, setStock] = useState(0);
+  const [unit, setUnit] = useState("");
+  
+  const [buttonLoading, setButtonLoading] = useState(false);
+
+  const handleAddItem = async () => {
+    setButtonLoading(true);
+
+    if (name.trim() === "") {
+      toast.error("Name cannot be empty.");
+      setButtonLoading(false);
+      return;
+    }
+
+    if (stock === null) {
+      toast.error("Stock cannot be empty.");
+      setButtonLoading(false);
+      return;
+    }
+
+    if (unit.trim() === "") {
+      toast.error("Unit cannot be empty.");
+      setButtonLoading(false);
+      return;
+    }
+
+    if (stock < 0) {
+      toast.error("Stock cannot be negative.");
+      setButtonLoading(false);
+      return;
+    }
+
+    if (stock % 1 !== 0) {
+      toast.error("Stock must be an integer.");
+      setButtonLoading(false);
+      return;
+    }
+
+    if (stock === 0) {
+      toast.error("Stock cannot be zero.");
+      setButtonLoading(false);
+      return;
+    }
+
+    if (unit.length > 10) {
+      toast.error("Unit cannot be more than 10 characters.");
+      setButtonLoading(false);
+      return;
+    }
+
+    if (name.length > 50) {
+      toast.error("Name cannot be more than 50 characters.");
+      setButtonLoading(false);
+      return;
+    }
+
+    try {
+      const newItem = {
+        name,
+        stock,
+        unit,
+        createdAt: serverTimestamp(),
+      };
+      await addDoc(collection(db, "items"), newItem);
+      setItems((prev) => [...prev, newItem]);
+      toast.success("Item added successfully");
+      setName("");
+      setStock(0);
+      setUnit("");
+    } catch (err) {
+      console.log(
+        "Error adding item",
+        "error: ",
+        err,
+        "error message: ",
+        err.message
+      );
+      toast.error("Error adding item");
+    }
+    setButtonLoading(false);
+  };
+
   return (
     <section>
       <h2>Add Item</h2>
-      <form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleAddItem();
+        }}
+      >
         <div>
-          <label htmlFor="name">Name</label>
-          <input type="text" id="name" />
+          <label htmlFor="name">Item Name</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            autoFocus
+            autoComplete="off"
+          />
         </div>
+
         <div>
           <label htmlFor="stock">Stock</label>
-          <input type="number" id="stock" />
+          <input
+            type="number"
+            id="stock"
+            name="stock"
+            value={stock}
+            onChange={(e) => setStock(e.target.value)}
+          />
         </div>
+
         <div>
           <label htmlFor="unit">Unit</label>
-          <input type="text" id="unit" />
+          <select
+            name="unit"
+            id="unit"
+            value={unit}
+            onChange={(e) => setUnit(e.target.value)}
+          >
+            <option value="" disabled selected>
+              Select a unit
+            </option>
+            <option value="kg">kg</option>
+            <option value="L">L</option>
+          </select>
         </div>
+
         <div>
-          <button type="submit">Add</button>
+          <button type="submit" disabled={buttonLoading}>
+            {buttonLoading ? (
+              <>
+                Adding... <ButtonSpinner />
+              </>
+            ) : (
+              "Add Item"
+            )}
+          </button>
         </div>
       </form>
     </section>
